ac4cd9daae2f3d1e9aada92d59103ea17934d066
Stefan Seritan <sseritan@gmail.com>
2019-03-15 14:52:39 -0700

    Exception catching for older TeraChem servers

--- tcpb/exceptions.py
+++ tcpb/exceptions.py
@@ -18,15 +18,15 @@ class ServerError(TCPBError):
 
         job_dir = client.curr_job_dir
         job_id = client.curr_job_id
-        if job_dir is not None and job_id is not None:
+        try:
             logfile = '{}/{}.log'.format(job_dir, job_id)
             with open(logfile, 'r') as logf:
                 lines = logf.readlines()
             
             debuglines = ''.join(lines[-10:])
             msg += 'Last 10 lines from logfile ({}):\n{}'.format(logfile, debuglines)
-        else:
-            msg += 'No current running job found, could not parse logfile'
+        except IOError, OSError:
+           msg += 'Could not open logfile' 
 
         super(ServerError, self).__init__(msg)
 
